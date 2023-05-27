c821a48f92667bd341749026457fd5697ec40659
Jamie <>
2023-05-27 20:49:00 +0800

    Merge branch '93-sign-in-with-google'

---
+++ src/utils/firebase/firebase.utils.jsx
@@ -0,0 +1,28 @@
+import { initializeApp } from 'firebase/app';
+import { 
+    getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider,
+} from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDT4v2HUR1iDtS5uRn43KGEJwkSzPx3fZk",
+  authDomain: "crwn-clothing-db-e33fc.firebaseapp.com",
+  projectId: "crwn-clothing-db-e33fc",
+  storageBucket: "crwn-clothing-db-e33fc.appspot.com",
+  messagingSenderId: "185462007066",
+  appId: "1:185462007066:web:4afcfef9677d3de667b8a3"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
