1535c3d545f60753ba2c160e3fb1f8d07dfcfd7e
Jamie <>
2023-05-28 15:09:37 +0800

    Merge branch '95-firestore'

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -6,6 +6,13 @@ import {
     GoogleAuthProvider,
 } from 'firebase/auth';
 
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+} from 'firebase/firestore';
+
 const firebaseConfig = {
   apiKey: "AIzaSyDT4v2HUR1iDtS5uRn43KGEJwkSzPx3fZk",
   authDomain: "crwn-clothing-db-e33fc.firebaseapp.com",
@@ -26,3 +33,14 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid)
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exist);
+}
