16ce3a307154e096f6940dfd39a9b26d4d55a4c4
Jamie <>
2023-05-29 21:26:14 +0800

    create user in firestore

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -42,5 +42,23 @@ export const createUserDocumentFromAuth = async (userAuth) => {
 
   const userSnapshot = await getDoc(userDocRef);
   console.log(userSnapshot);
-  console.log(userSnapshot.exist);
+  console.log(userSnapshot.exists());
+
+  if(!userSnapshot.exists()){
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    }catch(error){
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+
 }
