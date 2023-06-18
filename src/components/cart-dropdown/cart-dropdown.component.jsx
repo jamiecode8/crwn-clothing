1ba9caec3830caa239c6da55984b6e45ecaf2ca5
Jamie <>
2023-06-18 17:59:23 +0800

    fix className typo

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,7 +4,7 @@ import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
     return (
-        <div className='cart-dropdown-containrt'>
+        <div className='cart-dropdown-container'>
             <div className='cart-items'>
                 <Button>GO TO CHECKOUT</Button>
             </div>
