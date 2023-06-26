1ad78bea881db0594af00b0fde2781feae0f0b8b
Jamie <>
2023-06-26 22:54:34 +0800

    useNavigate and init checkout component

---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,28 @@
+import { useContext } from 'react'
+import { CartContext } from '../../contexts/cart.context'
+
+import './checkout.styles.scss'
+
+const Checkout = () => {
+    const { cartItems, addItemToCart } = useContext(CartContext)
+    return (
+        <div>
+            <h1>i am checkout page</h1>
+            <div>
+                {cartItems.map((cartItem)=>{
+                    const { id, name, quantity } = cartItem
+                    return (
+                        <div key={id}>
+                            <h2>{name}-{quantity}</h2>
+                            <span>decrement</span>
+                            <br/>
+                            <span onClick={()=>addItemToCart(cartItem)}>increment</span>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Checkout
