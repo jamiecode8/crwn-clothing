0398bfee586588de3c59b7c38b89457aab6ac059
Jamie <>
2023-06-26 21:09:52 +0800

    add cartCount in cartContext

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -8,13 +8,14 @@ import './cart-icon.styles.scss';
 
 const CartIcon = () => {
     const { isCartOpen, setIsCartOpen } = useContext(CartContext)
+    const { cartCount } = useContext(CartContext)
 
     const toogleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
     return (
         <div className='cart-icon-container' onClick={toogleIsCartOpen}>
             <ShoppingIcon className='shopping-icon'/>
-            <span className='item-count'>0</span>
+            <span className='item-count'>{cartCount}</span>
         </div>
     )
 }
