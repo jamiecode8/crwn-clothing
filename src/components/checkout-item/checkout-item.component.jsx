64547d8bb179b49dd6f34746f3e7f6c7648d834f
Jamie <>
2023-07-02 16:40:21 +0800

    clear item in the same remove function

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react'
+import { CartContext } from '../../contexts/cart.context'
+
 import './checkout-item.styles.scss'
 
 const CheckoutItem = ({cartItem}) => {
+    const { removeItemFromCart } = useContext(CartContext)
     const { name, imageUrl, price, quantity } = cartItem;
 
     return (
@@ -11,8 +15,7 @@ const CheckoutItem = ({cartItem}) => {
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
             <span className='quantity'>{quantity}</span>
-            <div className='remove-button'>&#10005;</div>
-
+            <div onClick={()=>removeItemFromCart(cartItem,true)} className='remove-button'>&#10005;</div>
         </div>
     );
 };
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -19,18 +19,24 @@ export const addCartItem = (cartItems, productToAdd) => {
     return [ ...cartItems, {...productToAdd, quantity:1 } ]
 }
 
-export const removeCartItem = (cartItems, itemToRemove) => {
+export const removeCartItem = (cartItems, itemToRemove, isDelete) => {
     // find the cart Item to remove
     const existingCartItem = cartItems.find(
         (cartItem)=>cartItem.id === itemToRemove.id
     );
 
-    // check the quantity is equal to 1, if it is remove that item from the cart
-    if(existingCartItem.quantity === 1 ){
+    if(isDelete || existingCartItem.quantity === 1){
         return cartItems.filter((cartItem)=>
             cartItem.id !== itemToRemove.id
         )
     }
+
+    // check the quantity is equal to 1, if it is remove that item from the cart
+    // if(existingCartItem.quantity === 1 ){
+    //     return cartItems.filter((cartItem)=>
+    //         cartItem.id !== itemToRemove.id
+    //     )
+    // }
 
     // return back cartitems with matching cart item with reduced quantity
     return cartItems.map((cartItem)=>
@@ -66,8 +72,8 @@ export const CartProvider = ({children}) => {
         setCartItems(addCartItem(cartItems, productToAdd))
     }
 
-    const removeItemFromCart = (itemToRemove) => {
-        setCartItems(removeCartItem(cartItems, itemToRemove))
+    const removeItemFromCart = (itemToRemove, isDelete) => {
+        setCartItems(removeCartItem(cartItems, itemToRemove, isDelete))
     }
 
     const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount, removeItemFromCart };
